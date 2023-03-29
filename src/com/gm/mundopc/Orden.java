681d9968c5fcb13ed7dae71d2854886b6ac53fc2
ridavao <rvobreque@gmail.com>
2023-03-29 01:35:16 -0300

    Se crean las clases Orden y MundoPC

---
+++ src/com/gm/mundopc/Orden.java
@@ -0,0 +1,22 @@
+
+package com.gm.mundopc;
+
+public class Orden {
+    private int idOrden;
+    private Computadora computadoras[];
+    private int contadorOrdenes;
+    private int contadorComputadoras;
+    private int maxComputadoras;
+    
+    public Orden(){
+        this.idOrden = ++this.contadorOrdenes;
+    }
+    
+    public void agregarComputadora(Computadora computadora){
+        
+    }
+    
+    public void mostrarOrden(){
+        
+    }
+}
---
+++ src/mundopc/MundoPC.java
@@ -0,0 +1,8 @@
+
+package mundopc;
+
+public class MundoPC {
+    public static void main(String[] args) {
+        
+    }
+}
