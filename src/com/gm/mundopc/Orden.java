ce0bb7f393bd76a98c4013c4afc5dd22dd9abd39
ridavao <rvobreque@gmail.com>
2023-03-29 19:51:23 -0300

    Update clase Orden y MundoPC

--- src/com/gm/mundopc/Orden.java
+++ src/com/gm/mundopc/Orden.java
@@ -26,7 +26,7 @@ public class Orden {
     
     public void mostrarOrden(){
         System.out.println("Orden #:" + this.idOrden);
-        System.out.println("Computadoras de la orden #:" + this.idOrden);
+        System.out.println("Computadoras de la orden #:" + this.contadorComputadoras);
         for (int i = 0; i < this.contadorComputadoras; i++) {
             System.out.println(this.computadoras[i]);
         }
--- src/mundopc/MundoPC.java
+++ src/mundopc/MundoPC.java
@@ -14,9 +14,15 @@ public class MundoPC {
         Monitor monitor1 = new Monitor("HP", 14.00);
         Computadora computadora1 = new Computadora("Gamer",monitor1, teclado1, raton1);
         
+        Teclado tecladoGamer = new Teclado("USB", "Teclado Gamer");
+        Raton raton2 = new Raton("USB", "Mouse HP");
+        Monitor monitorGamer = new Monitor("MSI", 33.00);
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, teclado1, raton2);
+        
         Orden orden1 = new Orden();
         System.out.println("orden1 " + orden1);
         orden1.agregarComputadora(computadora1);
+        orden1.agregarComputadora(computadoraGamer);
         orden1.mostrarOrden();
         
     }
