e2faff04c3dc6ad4e49daeba79c865208995485b
ridavao <rvobreque@gmail.com>
2023-03-29 00:46:13 -0300

    Se crea la clase Monitor

---
+++ src/com/gm/mundopc/Monitor.java
@@ -0,0 +1,63 @@
+
+package com.gm.mundopc;
+
+public class Monitor {
+    private int idMonitor;
+    private String marca;
+    private double tamanio;
+    private int contadorMonitores;
+    
+    public Monitor(){
+        this.idMonitor = ++this.contadorMonitores;
+    }
+    
+    public Monitor(String marca, double tamanio){
+        this();
+        this.marca = marca;
+        this.tamanio = tamanio;
+    }
+
+    public int getIdMonitor() {
+        return idMonitor;
+    }
+
+    public void setIdMonitor(int idMonitor) {
+        this.idMonitor = idMonitor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public int getContadorMonitores() {
+        return contadorMonitores;
+    }
+
+    public void setContadorMonitores(int contadorMonitores) {
+        this.contadorMonitores = contadorMonitores;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Monitor{idMonitor=").append(idMonitor);
+        sb.append(", marca=").append(marca);
+        sb.append(", tamanio=").append(tamanio);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
